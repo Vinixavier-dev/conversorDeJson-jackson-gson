753047fac9012bdac6eaf8e8254095de4c6bffcb
Vinicius <viniciusrodriguesx2005@gmail.com>
2025-02-08 19:25:47 -0300

    exercicios de contagem concluido. exercicio sobre a Tarefa: foi realizado os dois tipos de conversao (de Json para objeto e de objeto para File.json)

--- contador/src/main/java/br/com/alura/contador/tarefas/model/PrincipalTarefa.java
+++ contador/src/main/java/br/com/alura/contador/tarefas/model/PrincipalTarefa.java
@@ -3,9 +3,11 @@ package br.com.alura.contador.tarefas.model;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.expression.TargetedAccessor;
 
 import br.com.alura.contador.model.ContadorApplication;
 import br.com.alura.contador.tarefas.service.ConversorJacksonParaJson;
+import br.com.alura.contador.tarefas.service.ConversorJacksonParaObjeto;
 import br.com.alura.contador.tarefas.service.Documento;
 
 @SpringBootApplication
@@ -19,6 +21,7 @@ public class PrincipalTarefa implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Tarefa tarefa = new Tarefa(true, "codar em java e spring", "vinicius");
         ConversorJacksonParaJson conversorJackson = new ConversorJacksonParaJson();
+        ConversorJacksonParaObjeto conversorJacksonParaObjeto = new ConversorJacksonParaObjeto();
         var documento = new Documento();
 
         String ObjetoParaJson = conversorJackson.obterDados(tarefa);
@@ -27,7 +30,8 @@ public class PrincipalTarefa implements CommandLineRunner {
         System.out.println("Objeto Java para Json: ");
         System.out.println(ObjetoParaJson);
 
+        conversorJacksonParaObjeto.ConversorJacksonParaObjeto(documento);
         System.out.println("File Json para Objeto Java: ");
-        
+        System.out.println();
     }
 }
